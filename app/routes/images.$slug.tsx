011576a4b149e4b13e114d25f4d26dfb12fd2ac4
rutko <rutko@rutkos-MacBook-Air.local>
2023-07-24 14:47:28 +0900

    :+1:タグの紐付け

--- app/routes/images.$slug.tsx
+++ app/routes/images.$slug.tsx
@@ -6,6 +6,7 @@ import type { InferModel } from 'drizzle-orm';
 import { createClient } from "~/db/client.server"
 import { images, tags, imagesToTags } from '~/db/schema';
 import { eq } from "drizzle-orm";
+import Select from 'react-select';
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -18,13 +19,16 @@ type NewImagesToTags = InferModel<typeof imagesToTags, 'insert'>;
 export async function action({ params, request, context }: ActionArgs) {
   const imageId = Number(params.slug)
   const formData = await request.formData();
-  const tagId = Number(formData.get('tag'));
-  const newImagesToTags: NewImagesToTags = {
-    image_id: imageId,
-    tag_id: tagId,
+  const tagIds = formData.getAll('tags').map(Number);
+	const db = createClient(context.env.DB as D1Database);
+
+	for (const tagId of tagIds) {
+    const newImagesToTags: NewImagesToTags = {
+      image_id: imageId,
+      tag_id: tagId,
+    }
+    await db.insert(imagesToTags).values(newImagesToTags).run();
   }
-  const db = createClient(context.env.DB as D1Database);
-  await db.insert(imagesToTags).values(newImagesToTags).run();
   return redirect(`/images/${params.slug}`);
 }
 
@@ -32,18 +36,19 @@ export const loader = async ({ params, context }: LoaderArgs) => {
   const db = createClient(context.env.DB as D1Database);
   const imageId = params.slug
   const image = await db.select().from(images).where(eq(images.id, imageId)).all()
+	const allTags = await db.select().from(tags).all()
   if (!image) {
     throw new Response("Not Found", {
       status: 404,
     });
   }
-  return { image: image }
+  return { image: image, tags: allTags }
 }
 
-export default function TagSlug() {
+export default function ImageSlug() {
   const data = useLoaderData<typeof loader>();
   const imageName = data.image[0].name
-  console.log(data)
+	const tagOptions = data.tags.map(tag => ({ value: tag.id, label: tag.name }))
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>{ imageName }</h1>
@@ -56,11 +61,14 @@ export default function TagSlug() {
       <form method="post">
         <fieldset>
           <legend>{ imageName }の編集</legend>
-          <img src={`https://pub-4129228c5de34ed7a5eb7e59e41f2eae.r2.dev/${data.image[0].key}`} width="150" height="150"></img>
-          <div>
-            <label htmlFor="tag">タグ</label>
-            <input name="tag" type="text" required />
-          </div>
+          <img src={`https://pub-4129228c5de34ed7a5eb7e59e41f2eae.r2.dev/${data.image[0].key}`} height="150"></img>
+          <Select
+						isMulti
+						name="tags"
+						options={tagOptions}
+						className="basic-multi-select"
+						classNamePrefix="select"
+					/>
 
           <button type="submit">更新</button>
         </fieldset>
