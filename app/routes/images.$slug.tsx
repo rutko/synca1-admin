203b53da202c35adf06f81daf46e0f109e89076a
rutko <rutko@rutkos-MacBook-Air.local>
2023-07-25 20:29:23 +0900

    Revert

--- app/routes/images.$slug.tsx
+++ app/routes/images.$slug.tsx
@@ -7,7 +7,6 @@ import { createClient } from "~/db/client.server"
 import { images, tags, imagesToTags } from '~/db/schema';
 import { eq } from "drizzle-orm";
 import Select from 'react-select';
-import { useState, useEffect } from 'react';
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -20,32 +19,15 @@ type NewImagesToTags = InferModel<typeof imagesToTags, 'insert'>;
 export async function action({ params, request, context }: ActionArgs) {
   const imageId = Number(params.slug)
   const formData = await request.formData();
-  const newTagIds = formData.getAll('tags').map(Number);
+  const tagIds = formData.getAll('tags').map(Number);
 	const db = createClient(context.env.DB as D1Database);
 
-	// Get existing tag ids for the image
-  const existingImageToTags = await db.select().from(imagesToTags).where(eq(imagesToTags.image_id, imageId)).all();
-  const existingTagIds = existingImageToTags.map(it => it.tag_id);
-
-	// Find tag ids to be added and removed
-  const tagIdsToAdd = newTagIds.filter(id => !existingTagIds.includes(id));
-  const tagIdsToRemove = existingTagIds.filter(id => !newTagIds.includes(id));
-
-	// Add new tags
-  for (const tagId of tagIdsToAdd) {
+	for (const tagId of tagIds) {
     const newImagesToTags: NewImagesToTags = {
       image_id: imageId,
       tag_id: tagId,
     }
     await db.insert(imagesToTags).values(newImagesToTags).run();
-  }
-
-	// Remove tags
-  for (const tagId of tagIdsToRemove) {
-    const imageToTagToRemove = existingImageToTags.find(it => it.tag_id === tagId);
-    if (imageToTagToRemove) {
-      await db.delete(imagesToTags).where(eq(imagesToTags.tag_id, imageToTagToRemove.tag_id)).run();
-    }
   }
   return redirect(`/images/${params.slug}`);
 }
@@ -69,16 +51,7 @@ export default function ImageSlug() {
   const imageName = data.image[0].name
 	const tagOptions = data.tags.map(tag => ({ value: tag.id, label: tag.name }))
 	const selectedTagIds = data.imageToTags.map(it => it.tag_id);
-	const initialSelectedTags = tagOptions.filter(option => selectedTagIds.includes(option.value));
-	const [selectedTags, setSelectedTags] = useState(initialSelectedTags);
-
-	useEffect(() => {
-    setSelectedTags(initialSelectedTags);
-  }, [initialSelectedTags]);
-
-  const handleTagChange = (selectedOptions) => {
-    setSelectedTags(selectedOptions);
-  }
+  const selectedTags = tagOptions.filter(option => selectedTagIds.includes(option.value));
 	console.log(data)
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
@@ -94,14 +67,13 @@ export default function ImageSlug() {
           <legend>{ imageName }の編集</legend>
           <img src={`https://pub-4129228c5de34ed7a5eb7e59e41f2eae.r2.dev/${data.image[0].key}`} height="150"></img>
           <Select
-            isMulti
-            name="tags"
-            options={tagOptions}
-            className="basic-multi-select"
-            classNamePrefix="select"
-            value={selectedTags}
-            onChange={handleTagChange}
-          />
+						isMulti
+						name="tags"
+						options={tagOptions}
+						className="basic-multi-select"
+						classNamePrefix="select"
+						value={selectedTags}
+					/>
 
           <button type="submit">更新</button>
         </fieldset>
